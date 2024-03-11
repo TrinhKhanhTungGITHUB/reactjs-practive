e4d54c13123bf6fa2a86a7ed1160af840c92aee9
TungIT <tungdrivehaui01@gmail.com>
2024-03-11 16:31:42 +0700

    Design App Layout

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import Container from "react-bootstrap/Container"
 import Header from './components/Header';
 import TableUsers from './components/TableUsers';
 import { ToastContainer } from 'react-toastify';
+import Home from './components/Home';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
 
   return (
-    <div className='app-container'>
-      <Header />
-      <Container>
-      
-        <TableUsers />
-      </Container>
+    <>
+      <div className='app-container'>
+        <Header />
+        <Container>
+
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/users' element={<TableUsers />} />
+          </Routes>
+        </Container>
+
+      </div>
+
       <ToastContainer
         position="top-right"
         autoClose={5000}
@@ -23,8 +32,9 @@ function App() {
         pauseOnFocusLoss
         draggable
         pauseOnHover
-        />
-    </div>
+      />
+    </>
+
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,29 @@ import Container from 'react-bootstrap/Container';
 
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { NavLink, useLocation } from 'react-router-dom';
+import logoApp from '../assets/images/logo192.png';
 
 function Header(props) {
     return (<>
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="/">
+                    <img src={logoApp} width="30" height="30" className="d-inline-block align-top"
+                        alt="Logo"
+                    />
+                    <span>React-Bootstrap</span>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">  Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        <NavLink to="/" className="nav-link">Home</NavLink>
+                        <NavLink to="/users" className="nav-link"> Manage Users</NavLink>
+                    </Nav>
+                    <Nav>
+                        <NavDropdown title="Setting">
+                            <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+                            <NavDropdown.Item href="/logout">  Logout</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
