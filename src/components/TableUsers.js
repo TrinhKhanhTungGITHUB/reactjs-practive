b65cee5ebb2fff7a1c356f25469cf07cceae4636
TungIT <tungdrivehaui01@gmail.com>
2024-03-11 10:14:58 +0700

    Filter Users

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -4,8 +4,8 @@ import { fetchAllUser } from '../services/UserService';
 import ReactPaginate from 'react-paginate';
 import ModalAddNew from './ModalAddNew';
 import ModalEditUser from './ModalEditUser';
-// eslint-disable-next-line no-unused-vars
 import _ from "lodash";
+import { debounce } from 'lodash';
 import ModalConfirm from './ModalConfirm';
 import './TableUser.scss';
 
@@ -24,6 +24,8 @@ const TableUsers = (props) => {
 
     const [sortBy, setSortBy] = useState('asc')
     const [sortField, setSortFiled] = useState('id')
+
+    const [keyword, setKeyword] = useState('');
 
     useEffect(() => {
         getUsers(1);
@@ -93,11 +95,33 @@ const TableUsers = (props) => {
         setListUsers(cloneListUsers);
     }
 
+    //Search
+    const handleSearch = debounce((event) => {
+        let term = event.target.value;
+        if (term) {
+            let cloneListUsers = _.cloneDeep(listUsers);
+            cloneListUsers = cloneListUsers.filter(item => item.email.includes(term))
+            setListUsers(cloneListUsers);
+        }
+        else {
+            getUsers(1);
+        }
+    }, 500)
     return (
         <>
             <div className='my-3 add-new'>
                 <span><b>List Users:</b></span>
                 <button className='btn btn-success' onClick={handleClick}>Add New User</button>
+            </div>
+            <div className='col-4 my-3'>
+                <input
+                    className='form-control'
+                    placeholder='Search users by email ...'
+                    // value={ keyword}
+                    onChange={(event) => {
+                        handleSearch(event)
+                    }}
+                />
             </div>
             <Table striped bordered hover>
                 <thead>
